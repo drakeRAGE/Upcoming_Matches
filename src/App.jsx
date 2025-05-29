716199a72e8b983f713802050875bacedf5c1e0f
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2025-05-30 00:24:17 +0530

    First commit - Configuration of React + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='h-screen text-purple-500 bg-pink-300 flex items-center justify-center font-serif text-3xl'>Hlo, I am Drag !!</p>
+    </>
+  )
+}
+
+export default App
